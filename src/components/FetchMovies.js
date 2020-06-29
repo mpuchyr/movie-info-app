b9b2b61ff3b2879311c60e79a8acb53835b1dbac
mpuchyr <matt.puchyr@gmail.com>
2020-06-29 17:27:46 -0500

    Remove starter code in App.js

--- src/components/FetchMovies.js
+++ src/components/FetchMovies.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function FetchMovies () {
-    console.log('connected')
+
     
     return (
         <div>
+            <h1>Placeholder</h1>
         </div>
     )
 }
