5ce1941eb9416a0e961b4236ea441892fa11b211
mpuchyr <matt.puchyr@gmail.com>
2020-06-30 20:02:05 -0500

    Add search form to containers/SearchMovies

--- src/containers/SearchMovies.js
+++ src/containers/SearchMovies.js
@@ -3,11 +3,21 @@ import FetchMovies from '../components/FetchMovies'
 
 function SearchMovies() {
     
+    const [query, setQuery] = useState(null)
 
+    function userSearchQuery(event) {
+        event.preventDefault()
+        setQuery(event.target.name.value)
+        event.target.name.value = ''
+    }
 
     return (
         <div>
-            <FetchMovies searchQuery={'star wars'} />
+            <FetchMovies searchQuery={query} />
+            <form onSubmit={(event) => userSearchQuery(event)}>
+                <input type="text" name="name"></input>
+                <input type="submit"></input>
+            </form>
         </div>
     )
 }
