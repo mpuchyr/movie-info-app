750df6e393467ea1dfc44a93402b8104c8eb02cd
mpuchyr <matt.puchyr@gmail.com>
2020-06-30 19:32:42 -0500

    Change x-rapidapi-key in components/FetchMovies.js to an environment variable

--- src/components/FetchMovies.js
+++ src/components/FetchMovies.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
+const privateKey = process.env.REACT_APP_API_KEY
+const host = 'imdb8.p.rapidapi.com'
+
 function FetchMovies () {
+    fetch("https://imdb8.p.rapidapi.com/title/auto-complete?q=game%20of%20thr", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": `${host}`,
+		"x-rapidapi-key": `${privateKey}`
+	}
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(info => console.log(info))
+    .catch(err => {
+        console.log(err);
+    });
 
     
     return (
